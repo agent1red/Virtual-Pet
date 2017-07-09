ffaa73fe3608906dfab53fa3b507015a5f5a47aa
agent1red <kevinhudson@Kevins-MacBook-Pro.local>
2017-07-09 13:52:06 -0700

    Started with Virtual pet with phaserJS

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// This game will have 1 state
+
+var game = new Phaser.Game(640,360,AUTO);
+this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+this.scale.pageAlignHorizontally = true;
+this.scale.pageAlignVertically = true;
+
+var GameState = {
+  preload: function() {
+
+  },
+
+  create: function() {
+
+  },
+
+  update: function() {
+
+  },
+};
