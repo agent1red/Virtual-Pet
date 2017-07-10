011baaae90a95622fc8dc6f16d07b20cd15dee55
agent1red <kevinhudson@Kevins-MacBook-Pro.local>
2017-07-10 13:50:43 -0700

    Added progress bar functionality - game loads to game state

--- js/states/BootState.js
+++ js/states/BootState.js
@@ -1,1 +1,25 @@
-var BootState = {};
+var BootState = {
+
+  init: function() {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+  },
+
+  preload: function() {
+    // Load bootstate images (logo of game image and loading bar image)
+    this.load.image('logo', 'assets/images/logo.png');
+    this.load.image('preloadBar', 'assets/images/bar.png');
+
+
+  },
+
+  create: function() {
+
+    // set the background to white
+    this.game.stage.backgroundColor = '#fff';
+    // Show the loading screen
+    this.state.start('PreloadState');
+  }
+
+};
