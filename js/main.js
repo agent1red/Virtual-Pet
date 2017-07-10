890227c3b22a0d2ff32ead68a6aca4e7d128723c
agent1red <kevinhudson@Kevins-MacBook-Pro.local>
2017-07-09 20:57:27 -0700

    Added object selection

--- js/main.js
+++ js/main.js
@@ -36,26 +36,67 @@
    create: function() {
       this.background = this.game.add.sprite(0,0,'backyard');
 
+
+
       this.pet = this.game.add.sprite(100, 400, 'pet');
       this.pet.anchor.setTo(0.5);
 
       // setting up custom paramaters for the pet like health and happiness levels
 
-      this.pet.customParams = {health: 100, fun: 100}; 
+      this.pet.customParams = {health: 100, fun: 100};
 
-      this.apple = this.game.add.sprite(30, 570, 'apple');
-      this.candy = this.game.add.sprite(210, 570, 'candy');
-      this.toy = this.game.add.sprite(120, 570, 'toy');
-      this.rotate = this.game.add.sprite(300, 570, 'rotate');
-
+      //drag and drop objects
+      this.pet.inputEnabled = true;
+      this.pet.input.enableDrag();
 
 
+      // bottom elements of game
+      this.apple = this.game.add.sprite(53, 570, 'apple');
+      this.apple.anchor.setTo(0.5);
+      this.apple.inputEnabled = true;
+      this.apple.customParams = {health: 20};
+      this.apple.events.onInputDown.add(this.pickItem, this);
+
+      this.candy = this.game.add.sprite(223, 570, 'candy');
+      this.candy.anchor.setTo(0.5);
+      this.candy.inputEnabled = true;
+      this.candy.customParams = {health: -10, fun: 10};
+      this.candy.events.onInputDown.add(this.pickItem, this);
+
+      this.toy = this.game.add.sprite(133, 570, 'toy');
+      this.toy.anchor.setTo(0.5);
+      this.toy.inputEnabled = true;
+      this.toy.customParams = {fun: 20};
+      this.toy.events.onInputDown.add(this.pickItem, this);
+
+      this.rotate = this.game.add.sprite(323, 570, 'rotate');
+      this.rotate.anchor.setTo(0.5);
+      this.rotate.inputEnabled = true;
+      this.rotate.events.onInputDown.add(this.rotatePet, this);
+
+      // adding buttons to the object sprites in an array
+
+      this.buttons = [this.apple, this.candy, this.toy, this.rotate];
+      
 
    },
+
+// New function pick item passing the sprite object and event listener
+   pickItem: function(sprite, event) {
+
+      console.log('item picked');
+
+   },
+
+      // New function pick item passing the sprite object and event listener
+         rotatePet: function(sprite, event) {
+
+            console.log('Object rotated');
+         },
 
    update: function() {
 
-   },
+   }
 
 
  };
